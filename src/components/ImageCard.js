57b2d1244675f435dd716e35e54c6b0274f3eab7
Hano <hinsul3113@gmail.com>
2019-02-13 02:24:23 -0800

    added styles and proper height and width to each image

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+
+    // this is how we create refs(references) in react, we just call(React.createRef()) on it and assign in to a variable for later reference
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // console.log(this.imageRef);
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
+  }
+
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
-        <img alt={description} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}`}}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
